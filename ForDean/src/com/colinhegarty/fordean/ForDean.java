0c245bea8a308b9cae79e57a71b8122985f2010f
ColinHegarty <colin.hegarty4@mail.dcu.ie>
2016-02-29 11:52:00 +0000

    For Dean

---
+++ ForDean/src/com/colinhegarty/fordean/ForDean.java
@@ -0,0 +1,10 @@
+package com.colinhegarty.fordean;
+
+public class ForDean {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is for dean");
+	}
+
+}
