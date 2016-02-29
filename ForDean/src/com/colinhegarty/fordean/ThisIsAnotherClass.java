70db561c82b655a288f7dc66584e93d3e81d957e
ColinHegarty <colin.hegarty4@mail.dcu.ie>
2016-02-29 12:16:55 +0000

    second commit

---
+++ ForDean/src/com/colinhegarty/fordean/ThisIsAnotherClass.java
@@ -0,0 +1,10 @@
+package com.colinhegarty.fordean;
+
+public class ThisIsAnotherClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("New Class");
+	}
+
+}
